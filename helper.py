1d3f0a4f9c54d8c32210cc080b511d95d7c9a989
Pierre Berthe <42767265+MrTsunami4@users.noreply.github.com>
2024-01-05 10:29:53 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,13 +1,7 @@
-"""Quick helper functions for the project."""
-
-import typing
-
-if typing.TYPE_CHECKING:
-    from collections.abc import Iterator
+from typing import Iterator
 
 
 def nth_element(my_iter: Iterator, n: int) -> int:
-    """Return the nth element of an iterator."""
     for _ in range(n):
         next(my_iter)
     return next(my_iter)
